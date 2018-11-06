cd9cf12c27df8b726b06bd551b604ed07407e7f2
JosseandroFarias <farias.josseandro@gmail.com>
2018-11-06 17:06:08 -0400

    Adição de comentários para facilitar compreenção

--- web/index.php
+++ web/index.php
@@ -10,11 +10,7 @@
         <title>LoCar - Locadora de veículos</title>
 
         <!--<link href="dados/css/materialize.min.css" rel="stylesheet">-->
-        <link rel="stylesheet" href="dados/css/bootstrap.min.css">
-        <link rel="stylesheet" href="dados/css/font-awesome.min.css">
-        <link rel="stylesheet" href="dados/css/AdminLTE.min.css">
-        <link rel="stylesheet" href="dados/css/_all-skins.min.css">
-        <link rel="stylesheet" href="dados/css/site.css">
+        <?php include './CSSLink.php';?>
 
     </head>
 
@@ -22,90 +18,12 @@
 
         <div class="wrapper" style="height: auto; min-height: 100%">
 
-            <header class="main-header">
-                <a class="logo" href="index.php">
-                    <span class="logo-mini"><b>LV</b></span>
-                    <span class="logo-lg"><b>Loc</b>Vec</span>
-                </a>
-                <nav class="navbar navbar-static-top">
-                    <a id="menu-toggle" href="#" data-toggle="push-menu" role="button">
-                        <span data-feather="menu"></span>
-                    </a>
-
-                    <div class="navbar-custom-menu">
-                        <ul class="row nav navbar-nav navbar-expand">
-                            <li class="col-9 dropdown user user-menu nav-item">
-                                <a class="dropdown-toggle" href="#" data-toggle="dropdown">
-                                    <!--<img class="user-image" src="imgcaminho">-->
-                                    <span data-feather="user"></span>
-                                    <span class="hidden-xs">Usuário</span>
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li class="user-body">Usuário: Master</li>
-                                    <li class="user-footer">
-                                        <div class="row">
-                                            <div class="col-sm-8">
-                                                <a class="btn btn-success btn-flat btn-block" href="?acao=logoff&amp;pgn=inicio">Ajustes</a>
-                                            </div>
-                                            <div class="col-sm-4">
-                                                <a class="btn btn-default btn-flat btn-block" href="?acao=sair&amp;pgn=inicio">Sair</a>
-                                            </div>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li class="col-3 nav-item">
-                                <a href="#" data-toggle="control-sidebar">
-                                    <span data-feather="settings"></span>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
-            </header>
-            
+            <!--Topo site-->
+            <?php include './topo.php'; ?>
             <!--Menu lateral-->
-            <aside class="main-sidebar">
-                <section class="sidebar" style="height: auto;">
-                    <ul class="sidebar-menu tree" data-widget="tree">
-                        <!--<li class="header">Opções de navegação</li>-->
-                        <li>
-                            <a href="index.php">
-                                <span data-feather="home"></span>
-                                <span>Locação</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="pages/clientes.php">
-                                <span data-feather="user"></span>
-                                <span>Clientes</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="pages/veiculo.php">
-                                <span data-feather="shopping-cart"></span>
-                                <span>Carros</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="pages/relatorios.php">
-                                <span data-feather="file"></span>
-                                <span>Relatórios</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="pages/cad_funcionarios.php">
-                                <span data-feather="users"></span>
-                                <span>Funcionários</span>
-                            </a>
-                        </li>
-                    </ul>
-                    <!--</div>-->
-                </section>
-                <!--</nav>-->
-            </aside>
+            <?php include './menuLateral.php'; ?>
 
-            <!--centro-->
+            <!--centro meio que irá mudar-->
             <div class="content-wrapper" style="min-height: 717px;">
                 <section class="content">
 
@@ -253,33 +171,11 @@
                     </div>
                 </section>
             </div>
-
-            <!--painel mudar de cor-->
-            <aside class="control-sidebar control-sidebar-dark">
-                <div>
-                    <div class="col-xs-12">
-                        <div id="sel_cores">
-                            <div id="control-sidebar-theme-demo-options-tab" class="tab-pane active">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </aside>
+            <!--finalização do meio--> 
             
-            <div class="control-sidebar-bg"></div>
+            <!--painel mudar de cor-->
+            <?php include './painelConfCor.php'; ?>
         </div>
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="dados/js/bootstrap.min.js" type="text/javascript"></script>
-        <script src="dados/js/adminlte.min.js" type="text/javascript"></script>
-        <script src="dados/js/demo.js" type="text/javascript"></script>
-        <script src="dados/js/bootbox.min.js" type="text/javascript"></script>
-
-        <!-- Icons -->
-        <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
-        <script>
-            feather.replace()
-        </script>
-        <!-- Graphs -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js"></script>
+        <?php include './JSLink.php';?>
     </body>
 </html>
