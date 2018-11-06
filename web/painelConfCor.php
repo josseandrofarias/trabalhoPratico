d8413e0027fe54fbb1e40005d86bf6c99d1b8480
JosseandroFarias <farias.josseandro@gmail.com>
2018-11-06 16:32:12 -0400

    Separação de Menu/topo/painel de cor para melhor estruturação

---
+++ web/painelConfCor.php
@@ -0,0 +1,12 @@
+<aside class="control-sidebar control-sidebar-dark">
+    <div>
+        <div class="col-xs-12">
+            <div id="sel_cores">
+                <div id="control-sidebar-theme-demo-options-tab" class="tab-pane active">
+                </div>
+            </div>
+        </div>
+    </div>
+</aside>
+
+<div class="control-sidebar-bg"></div>
---
+++ web/topo.php
@@ -0,0 +1,41 @@
+<header class="main-header">
+    <a class="logo" href="index.php">
+        <span class="logo-mini"><b>LV</b></span>
+        <span class="logo-lg"><b>Loc</b>Vec</span>
+    </a>
+    <nav class="navbar navbar-static-top">
+        <a id="menu-toggle" href="#" data-toggle="push-menu" role="button">
+            <span data-feather="menu"></span>
+        </a>
+
+        <div class="navbar-custom-menu">
+            <ul class="row nav navbar-nav navbar-expand">
+                <li class="col-9 dropdown user user-menu nav-item">
+                    <a class="dropdown-toggle" href="#" data-toggle="dropdown">
+                        <!--<img class="user-image" src="imgcaminho">-->
+                        <span data-feather="user"></span>
+                        <span class="hidden-xs">Usuário</span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li class="user-body">Usuário: Master</li>
+                        <li class="user-footer">
+                            <div class="row">
+                                <div class="col-sm-8">
+                                    <a class="btn btn-success btn-flat btn-block" href="?acao=logoff&amp;pgn=inicio">Ajustes</a>
+                                </div>
+                                <div class="col-sm-4">
+                                    <a class="btn btn-default btn-flat btn-block" href="?acao=sair&amp;pgn=inicio">Sair</a>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+                <li class="col-3 nav-item">
+                    <a href="#" data-toggle="control-sidebar">
+                        <span data-feather="settings"></span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
