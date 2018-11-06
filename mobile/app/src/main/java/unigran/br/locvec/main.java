b5d07816f3f9d855cf17057163ad9aacc87fd95e
Paulo Wosniak <paulohwff@gmail.com>
2018-11-06 16:34:03 -0200

    Criação da tela Relatorios em Android

--- mobile/app/src/main/java/unigran/br/locvec/main.java
+++ mobile/app/src/main/java/unigran/br/locvec/main.java
@@ -1,7 +1,9 @@
 package unigran.br.locvec;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class main extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickBtnRelatorios(View view){
+        Intent it = new Intent(main.this, Relatorios.class);
+        startActivity(it);
     }
 }
