5f943b09b55ded6b4409fa5c871605e917825917
JosseandroFarias <farias.josseandro@gmail.com>
2018-11-06 16:57:19 -0400

    Criação de arquivos para dar include no css e js

---
+++ web/JSLink.php
@@ -0,0 +1,13 @@
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="dados/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="dados/js/adminlte.min.js" type="text/javascript"></script>
+<script src="dados/js/demo.js" type="text/javascript"></script>
+<script src="dados/js/bootbox.min.js" type="text/javascript"></script>
+
+<!-- Icons -->
+<script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
+<script>
+    feather.replace()
+</script>
+<!-- Graphs -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js"></script>
