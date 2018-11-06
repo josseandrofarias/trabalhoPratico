134adfa1c2d43341617cb3bc6335679ffbaf2536
Alex <802.099@alunos.unigran.br>
2018-11-06 19:27:22 -0300

    Melhoria no interface

--- web/pages/cad_funcionarios.php
+++ web/pages/cad_funcionarios.php
@@ -91,6 +91,23 @@
   						<input class="form-check-input" type="checkbox" value="" id="ckbxSupervisor">
  						<label class="form-check-label" for="defaultCheck1">Supervisor</label>
 					</div>
+
+					<div class="well">
+  						<div id="datetimepicker2" class="input-append">
+    						<input data-format="MM/dd/yyyy HH:mm:ss PP" type="text"></input>
+    						<span class="add-on">
+     							<i data-time-icon="icon-time" data-date-icon="icon-calendar"> </i>
+   							</span>
+  						</div>
+					</div>
+					<script type="text/javascript">
+					  $(function() {
+					    $('#datetimepicker2').datetimepicker({
+					      language: 'en',
+					      pick12HourFormat: true
+					    });
+					  });
+					</script>
 					
 					<button type="button" class="btn btn-primary btn-sm">Editar</button>
 					
