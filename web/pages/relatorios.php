3c94e74830049b64fbb5ac3b37b5b2ae5431eefc
Paulo Wosniak <paulohwff@gmail.com>
2018-11-06 14:44:35 -0200

    Página de relatórios criada

--- web/pages/relatorios.php
+++ web/pages/relatorios.php
@@ -71,16 +71,16 @@
         <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
           <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
             <div class="input-group mb-3">
-                <h1 class="h2">Relatórios</h1>
+                <h1 class="h2">Gerar relatórios</h1>
             </div>
           </div>
 
             <div class="container-fluid">
               <div class="input-group mb-3">
                 <div class="input-group-prepend">
-                  <label class="input-group-text" for="inputGroupSelect01">Tipo</label>
+                  <label class="input-group-text" for="inputGroupSelect01"><strong>Tipo do relatório</strong></label>
                 </div>
-                <select class="custom-select" id="inputGroupSelect01">
+                <select class="custom-select col-2" id="inputGroupSelect01">
                   <option selected>Selecione...</option>
                   <option value="1">Km Carros</option>
                   <option value="2">Locações</option>
@@ -90,8 +90,27 @@
               </div>
             </div>
 
-              
+            <div class="container-fluid">
+              <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                  <label class="input-group-text" for="inputGroupSelect01"><strong>Data inicial</strong></label>
+                </div>
+               <input class="form-control col-2" type="date" value="" id="data_final">
+              </div>
+            </div>
+
+            <div class="container-fluid">
+              <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                  <label class="input-group-text" for="inputGroupSelect01"><strong>Data final</strong></label>
+                </div>
+               <input class="form-control col-2" type="date" value="" id="data_final">
+              </div>
+              <button type="button" class="btn btn-primary btn-lg" style="background-color: #545254; border-color: #000">Gerar relatório</button>
+            </div>
+
             
+
 
         </main>
       </div>
