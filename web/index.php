00438f5dfda7830b0f9cd2b283aa21e7de80eecc
Paulo Wosniak <paulohwff@gmail.com>
2018-11-06 10:49:13 -0200

    Alterações no layout da index.php

--- web/index.php
+++ web/index.php
@@ -7,11 +7,11 @@
     <meta name="description" content="">
     <meta name="Grupo2" content="">
 
-    <title>Dashboard Template for Bootstrap</title>
+    <title>LoCar - Locadora de veículos</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+    <link href="dados/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="dados/css/AdminLTE.min.css">
 
 
     <!-- Custom styles for this template -->
@@ -19,12 +19,11 @@
   </head>
 
   <body>
-    <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Company name</a>
-      <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+    <nav class="navbar navbar-dark bg-dark flex-md-nowrap p-0 shadow">
+      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="index.php">LoCar</a>
       <ul class="navbar-nav px-3">
         <li class="nav-item text-nowrap">
-          <a class="nav-link" href="#">Sign out</a>
+          <a class="nav-link" href="#">Sair</a>
         </li>
       </ul>
     </nav>
@@ -37,70 +36,31 @@
               <li class="nav-item">
                 <a class="nav-link active" href="#">
                   <span data-feather="home"></span>
-                  Dashboard <span class="sr-only">(current)</span>
+                  Locação<span class="sr-only">(current)</span>
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="file"></span>
-                  Orders
+                <a class="nav-link" href="pages/clientes.php">
+                  <span data-feather="user"></span>
+                  Clientes
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">
+                <a class="nav-link" href="pages/veiculo.php">
                   <span data-feather="shopping-cart"></span>
-                  Products
+                  Carros
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">
+                <a class="nav-link" href="pages/relatorios.php">
+                  <span data-feather="file"></span>
+                  Relatórios
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="pages/cad_funcionarios.php">
                   <span data-feather="users"></span>
-                  Customers
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="bar-chart-2"></span>
-                  Reports
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="layers"></span>
-                  Integrations
-                </a>
-              </li>
-            </ul>
-
-            <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-              <span>Saved reports</span>
-              <a class="d-flex align-items-center text-muted" href="#">
-                <span data-feather="plus-circle"></span>
-              </a>
-            </h6>
-            <ul class="nav flex-column mb-2">
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="file-text"></span>
-                  Current month
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="file-text"></span>
-                  Last quarter
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="file-text"></span>
-                  Social engagement
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="file-text"></span>
-                  Year-end sale
+                  Funcionários
                 </a>
               </li>
             </ul>
@@ -109,29 +69,29 @@
 
         <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
           <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-            <h1 class="h2">Dashboard</h1>
+            <h1 class="h2">Locação</h1>
             <div class="btn-toolbar mb-2 mb-md-0">
               <div class="btn-group mr-2">
-                <button class="btn btn-sm btn-outline-secondary">Share</button>
-                <button class="btn btn-sm btn-outline-secondary">Export</button>
+                <button class="btn btn-sm btn-outline-secondary">Cadastrar</button>
+                <button class="btn btn-sm btn-outline-secondary">Deletar</button>
+                <button class="btn btn-sm btn-outline-secondary">Editar</button>
               </div>
               <button class="btn btn-sm btn-outline-secondary dropdown-toggle">
                 <span data-feather="calendar"></span>
-                This week
+                Hoje
               </button>
             </div>
           </div>
 
-          <h2>Section title</h2>
           <div class="table-responsive">
             <table class="table table-striped table-sm">
               <thead>
                 <tr>
-                  <th>#</th>
-                  <th>Header</th>
-                  <th>Header</th>
-                  <th>Header</th>
-                  <th>Header</th>
+                  <th>id</th>
+                  <th>Data Locação</th>
+                  <th>Data Devolução</th>
+                  <th>Km</th>
+                  <th>Valor</th>
                 </tr>
               </thead>
               <tbody>
