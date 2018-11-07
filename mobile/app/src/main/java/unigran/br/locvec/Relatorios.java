42fe3bf21f40216f6df907d0ca52b78236c739e0
Paulo Wosniak <paulohwff@gmail.com>
2018-11-07 13:16:52 -0200

    Inserção do menu lateral nas telas Main e Relatórios ANDROID

--- mobile/app/src/main/java/unigran/br/locvec/Relatorios.java
+++ mobile/app/src/main/java/unigran/br/locvec/Relatorios.java
@@ -1,9 +1,7 @@
 package unigran.br.locvec;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -31,14 +29,7 @@ public class Relatorios extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -107,17 +98,18 @@ public class Relatorios extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_locacao) {
+            Intent it = new Intent(Relatorios.this, Main.class);
+            startActivity(it);
+        } else if (id == R.id.nav_clientes) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_carros) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_relatorios) {
+            Intent it = new Intent(Relatorios.this, Relatorios.class);
+            startActivity(it);
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_relatorios) {
 
         }
 
