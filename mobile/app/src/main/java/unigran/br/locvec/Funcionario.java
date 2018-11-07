c18b0482c6e2c30bddaf578b8fe0711639eaabe6
Alex <802.099@alunos.unigran.br>
2018-11-07 20:29:30 -0300

    Criação da Tela de Funcionarios

--- mobile/app/src/main/java/unigran/br/locvec/Funcionario.java
+++ mobile/app/src/main/java/unigran/br/locvec/Funcionario.java
@@ -1,6 +1,5 @@
 package unigran.br.locvec;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -14,15 +13,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class main extends AppCompatActivity
+public class Funcionario extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
-    static boolean active = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_funcionario);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -35,11 +32,6 @@ public class main extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-    }
-
-    public void clickBtnRelatorios(View view){
-        Intent it = new Intent(main.this, Relatorios.class);
-        startActivity(it);
     }
 
     @Override
@@ -55,7 +47,7 @@ public class main extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.funcionario, menu);
         return true;
     }
 
@@ -74,43 +66,24 @@ public class main extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        active = true;
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        active = false;
-        finish();
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_locacao) {
-            if(active){
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
 
-            }else{
-                Intent it = new Intent(main.this, main.class);
-                startActivity(it);
-            }
-        } else if (id == R.id.nav_clientes) {
+        } else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.nav_carros) {
+        } else if (id == R.id.nav_manage) {
 
-        } else if (id == R.id.nav_relatorios) {
-            Intent it = new Intent(main.this, Relatorios.class);
-            startActivity(it);
+        } else if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_funcionários) {
-            Intent it = new Intent(main.this, Funcionario.class);
-            startActivity(it);
+        } else if (id == R.id.nav_send) {
+
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
