c9b6335869e9d2ec115c9758f5edd88bd5e756ec
Alex <802.099@alunos.unigran.br>
2018-11-07 19:47:45 -0300

    Ajuste Final

--- web/pages/cad_funcionarios.php
+++ web/pages/cad_funcionarios.php
@@ -1,135 +1,105 @@
 
-<!DOCTYPE html>
+<?php
+include '../TFuncao.php';
+?>
 <html lang="pt-br">
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<meta name="description" content="">
-		<meta name="Grupo2" content="">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="Grupo2" content="">
 
-		<title>LoCar - Cadastro de Funcionários	</title>
+        <title>LoCar - Locadora de veículos</title>
 
-		<!-- Bootstrap core CSS -->
-		<link href="../dados/css/bootstrap.min.css" rel="stylesheet">
-		<link rel="stylesheet" href="../dados/css/AdminLTE.min.css">
+        <!--<link href="dados/css/materialize.min.css" rel="stylesheet">-->
+        <?php
+        echo TFuncoes::AddCss(false);
+        ?>
 
-		<!-- Custom styles for this template -->
-		<!--<link  rel="stylesheet" href="../dados/css/dashboard.css">-->
-	</head>
+    </head>
 
-	<body>
+    <body class="skin-blue">
 
+        <div class="wrapper" style="height: auto; min-height: 100%">
 
-		<nav class="navbar navbar-dark bg-dark flex-md-nowrap p-0 shadow">
-			<a class="navbar-brand col-sm-3 col-md-2 mr-0" href="index.php">LoCar</a>
-			<ul class="navbar-nav px-3">
-				<li class="nav-item text-nowrap">
-					<a class="nav-link" href="#">Sair</a>
-				</li>
-			</ul>
-		</nav>
+            <!--Topo site-->
+            <?php echo TFuncoes::AddTopo() ?>
+            <!--Menu lateral-->
+            <?php echo TFuncoes::AddMenuLateral(false);// include './dados/menuLateral.php'; ?>
 
-		<div class="container-fluid">
-			<div class="row">
+            <!--centro-->
+            <div class="content-wrapper" style="min-height: 717px;">
+               
+                    <!--SEU CODIGO AQUI DENTRO--> 
+                    <section class="content">
 
-				<nav class="col-md-2 d-none d-md-block bg-light sidebar">
-					<div class="sidebar-sticky">
-						<ul class="nav flex-column">
-							<li class="nav-item">
-								<a class="nav-link active" href="#">
-									<span data-feather="home"></span>
-									Locação<span class="sr-only">(current)</span>
-								</a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="pages/clientes.php">
-									<span data-feather="user"></span>
-									Clientes
-								</a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="pages/veiculo.php">
-									<span data-feather="shopping-cart"></span>
-									Carros
-								</a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="pages/relatorios.php">
-									<span data-feather="file"></span>
-									Relatórios
-								</a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="pages/cad_funcionarios.php">
-									<span data-feather="users"></span>
-									Funcionários
-								</a>
-							</li>
-						</ul>
-					</div>
-				</nav>
-
-				<main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
-
-					<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-						<h1 class="h2">Gerenciar Funcionários</h1>
-					</div>
+                    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                        <h1 class="h2">Gerenciar Funcionários</h1>
+                    </div>
 
                     <!-- ESCREVER CODIGO AQUI! -->
-					
-					<form>
-  						<div class="form-group">
-    						<input class="form-control form-control-lg" type="text" placeholder="Nome"></br/>
-    						<input class="form-control form-control-lg" type="text" placeholder="Endereço"></br/>
-    						<div class="row">
- 								<div class="col-md-3">
- 									<input class="form-control form-control-lg" type="text" placeholder="CPF"></br/>
- 								</div>
-  								<div class="col-md-3">
-  									<input class="form-control form-control-lg" type="text" placeholder="RG"></br/>	
-  								</div>
-							</div>
-    						
-    						
-    						<input class="form-control form-control-lg" type="text" placeholder="Cargo"></br/>
+                    
+                    <form>
+                        <div class="form-group">
+                            <input class="form-control form-control-lg" type="text" placeholder="Nome"></br/>
+                            <input class="form-control form-control-lg" type="text" placeholder="Endereço"></br/>
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <input class="form-control form-control-lg" type="text" placeholder="CPF"></br/>
+                                </div>
+                                <div class="col-md-4">
+                                    <input class="form-control form-control-lg" type="text" placeholder="RG"></br/> 
+                                </div>
+                                
+                                <div class="col-md-4">
+                                <input class="form-control form-control-lg" type="text" placeholder="Cargo"></br/>
+                            </div>
+                            </div>
 
-    						<div class="row">
- 								<div class="col-md-4">
- 									<input class="form-control form-control-lg" type="date" placeholder="Admissao"></br/>
- 								</div>
-  								<div class="col-md-4">
-  									<input class="form-control form-control-lg" type="date" placeholder="Admissao"></br/>	
-  								</div>
-							</div>
+                            <div class="row">
+                                <div class="col-md-4 ">
+                                	<p class="label">Data de Admissão</p>
+                                    <input class="form-control form-control-lg" type="date" placeholder="Admissao">
 
-    						
-    						
-  						</div>
-  						<div class="form-group form-check">
-    						<input type="checkbox" class="form-check-input" id="exampleCheck1">
-    						<label class="form-check-label" for="exampleCheck1">Check me out</label>
-  						</div>
-  						<button type="submit" class="btn btn-primary">Submit</button>
-					</form>
-					
-					
+                                </div>
+                                <div class="col-md-4">
+                                	<p class="label">Data de Demissão</p>
+                                    <input class="form-control form-control-lg" type="date" placeholder="Admissao">   
+                                </div>
+                            </div>
 
-				</main>
+                            
+                            
+                        </div>
 
-	 		</div>
-		</div>
+                        <div class="row">
+                                <div class="col-md-4">
+                                    <div class="form-group form-check">
+                                        <input type="checkbox" class="form-check-input" id="ckBoxSupervisor">
+                                        <label class="form-check-label" for="ckBoxSupervisor">Funcionário Supervisor</label>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="form-group form-check">
+                                        <input type="checkbox" class="form-check-input" id="ckBoxDesativado">
+                                        <label class="form-check-label" for="ckBoxDesativado">Funcionário Desativado</label>
+                                    </div>  
+                                </div>
+                            </div>
+                        
+                        <button type="submit" class="btn btn-primary">Salvar</button>
+                        <button type="submit" class="btn btn-primary">Editar</button>
+                    </form>
+                      
 
+                    <!--FINAL DO SEU CODIGO-->
+                </section>
+            </div>
+            <!--finalização do Centro--> 
 
-		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-		<script src="../dados/js/bootstrap.min.js"></script>
-
-		<!-- Icons -->
-		<script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
-		<script>
-	 		feather.replace()
-		</script>
-		<!-- Graphs -->
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js"></script>
-
-	</body>
+            <!--painel mudar de cor-->
+            <?php echo TFuncoes::AddPainelCor(); ?>
+        </div>
+        <?php echo TFuncoes::AddJs(false) ?>
+    </body>
 </html>
