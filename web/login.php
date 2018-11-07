624b6e5f05e40d085a82d6cb17ac797c0b3d5f2b
JosseandroFarias <farias.josseandro@gmail.com>
2018-11-07 16:23:56 -0400

    Beta de layout da page login finalizada

--- web/login.php
+++ web/login.php
@@ -11,68 +11,67 @@ include './TFuncao.php';
 
         <title>LoCar - Login</title>
 
-        <!--<link href="dados/css/materialize.min.css" rel="stylesheet">-->
-        <!--        <link rel="stylesheet" href="./dados/css/bootstrap.css">
-                <link rel="stylesheet" href="./dados/css/_all-skins.min.css">
-                <link rel="stylesheet" href="./dados/css/bootstrap.min.css">
-                <link rel="stylesheet" href="./dados/css/AdminLTE.min.css">
-                <link rel="stylesheet" href="./dados/css/font-awesome.min.css">
-                <link rel="stylesheet" href="./dados/css/site.css">-->
-
         <?php
         echo TFuncoes::AddCss(true);
         ?>
-        <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">-->
-        <!--<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">-->
 
+        <!--        <link rel="stylesheet" href="./dados/css/bootstrap.min.css">
+        <link rel="stylesheet" href="./dados/css/font-awesome.min.css">
+        <link rel="stylesheet" href="./dados/css/AdminLTE.min.css">
+        <link rel="stylesheet" href="./dados/css/_all-skins.min.css">
+                <link rel="stylesheet" href="./dados/css/site.css">
+                    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+                <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">-->
 
     </head>
     <!--<body class="loginBody">-->
-    <body class="hold-transition login-page">
+    <body class="login-page">
         <div class="login-box">
 
             <!--logo-->
             <div class="login-logo">
                 <a href="./index.php">
-                    <b>Lo</b>Car
+                    <b class="logo-lg">Lo</b>Car
                 </a>
             </div>
             <!--Form login-->
             <div class="login-box-body ">
-                <p class="login-box-msg">Entre com CPF e senha para iniciar suas vendas</p>
+                <p class="login-box-msg">Entre com CPF e Senha para iniciar suas vendas</p>
                 <form>
                     <div class="form-group has-feedback">
-                        <span data-feather="user"></span>
-                        <i class="fas fa-user"></i>
-                        <span class="glyphicon glyphicon-user"></span>
+                        <div class="input-group ">
+                            <span class="fas fa-user form-control-lg col-2"></span>
                             <input type="text" class="form-control" placeholder="CPF" required="required">
-                            </div>
-
-                            <div class="form-group">
-                                <span class="input-group-addon">
-                                    <!--<i class="fa fa-lock"></i>-->
-                                </span>
-                                <input type="password" class="form-control" placeholder="Senha" required="required">
-                            </div>
-
-                            <div class="form-group">
-                                <button type="submit" class="btn btn-primary login-btn btn-block">
-                                    Entrar
-                                </button>
-                            </div>
-
-                            <div class="clearfix">
-                                <label class="pull-left checkbox-inline"><input type="checkbox">Lembrar dados</label>
-                            </div>
-                            </form>
+                        </div>
                     </div>
-            </div>
 
-            <?php
-            echo TFuncoes::AddJs(true);
-            ?>
-            <!--<script src="./dados/js/site.js" type="text/javascript"></script>-->
-            <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">-->
+                    <div class="form-group has-feedback ">
+                        <div class="input-group">
+                            <span class="fas fa-key form-control-lg col-2"></span>
+                            <input type="password" class="form-control" placeholder="Senha" required="required">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary login-btn btn-block">
+                            Entrar
+                        </button>
+                    </div>
+
+                    <div class="clearfix">
+                        <label class="pull-left checkbox-inline"><input type="checkbox">Lembrar dados</label>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+        <?php
+        echo TFuncoes::AddJs(true);
+        ?>
+           <!--<script defer src="https://use.fontawesome.com/releases/v5.5.0/js/solid.js" integrity="sha384-Xgf/DMe1667bioB9X1UM5QX+EG6FolMT4K7G+6rqNZBSONbmPh/qZ62nBPfTx+xG" crossorigin="anonymous"></script>-->
+<!--<script defer src="https://use.fontawesome.com/releases/v5.5.0/js/regular.js" integrity="sha384-XrvTJeiQ46fxxPrZP6fay5yejA2FV4G1XsS8E4Piz6Fz+7FaEFTw7A7GR972irVV" crossorigin="anonymous"></script>-->
+<!--<script defer src="https://use.fontawesome.com/releases/v5.5.0/js/brands.js" integrity="sha384-S2C955KPLo8/zc2J7kJTG38hvFV+SnzXM6hwfEUhGHw5wPo6uXbnbjSJgw3clO4G" crossorigin="anonymous"></script>-->
+<!--<script defer src="https://use.fontawesome.com/releases/v5.5.0/js/fontawesome.js" integrity="sha384-bNOdVeWbABef8Lh4uZ8c3lJXVlHdf8W5hh1OpJ4dGyqIEhMmcnJrosjQ36Kniaqm" crossorigin="anonymous"></script>-->
     </body>
 
 </html>
