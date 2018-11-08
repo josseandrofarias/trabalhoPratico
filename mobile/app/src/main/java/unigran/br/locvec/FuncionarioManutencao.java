7f817fe2fa95070d849632fafcd3726c8603a420
Alex <802.099@alunos.unigran.br>
2018-11-07 21:58:10 -0300

    Correções layout e botões

--- mobile/app/src/main/java/unigran/br/locvec/FuncionarioManutencao.java
+++ mobile/app/src/main/java/unigran/br/locvec/FuncionarioManutencao.java
@@ -10,4 +10,5 @@ public class FuncionarioManutencao extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_funcionario_manutencao);
     }
+
 }
