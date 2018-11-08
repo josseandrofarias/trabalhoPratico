38ff89f24eccd4d298869081c96bb8cf73aeb7e5
Alex <802.099@alunos.unigran.br>
2018-11-07 21:12:09 -0300

    Finalizacao do tela

---
+++ mobile/app/src/main/java/unigran/br/locvec/FuncionarioManutencao.java
@@ -0,0 +1,13 @@
+package unigran.br.locvec;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FuncionarioManutencao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_funcionario_manutencao);
+    }
+}
