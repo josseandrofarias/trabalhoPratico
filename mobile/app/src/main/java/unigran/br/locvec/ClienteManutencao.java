d8f2a285e344dfe4e81dbf877e35a3ef3472fa64
Igor Ramos <knuhhgfrdcgy@gmail.com>
2018-11-07 22:22:39 -0300

    Criação de tela de listar cliente e tela de cadastrar clinte ANDROID

---
+++ mobile/app/src/main/java/unigran/br/locvec/ClienteManutencao.java
@@ -0,0 +1,13 @@
+package unigran.br.locvec;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClienteManutencao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cliente_manutencao);
+    }
+}
