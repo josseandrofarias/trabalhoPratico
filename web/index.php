121b7e9cb90278a5d99b295ec3c68d26011a614a
João Vitor Morosini <jvm-nogueira@hotmail.com>
2018-11-09 16:41:15 -0300

    Alteração de layout e adição de modals index.php

--- web/index.php
+++ web/index.php
@@ -1,7 +1,5 @@
 
-<?php
-include './TFuncao.php';
-?>
+<?php include './TFuncao.php'; ?>
 <html lang="pt-br">
     <head>
         <meta charset="utf-8">
@@ -9,12 +7,9 @@ include './TFuncao.php';
         <meta name="description" content="">
         <meta name="Grupo2" content="">
 
-        <title>LoCar - Locadora de veículos</title>
+        <title>LoCar - Locadora de Veículos</title>
 
-        <!--<link href="dados/css/materialize.min.css" rel="stylesheet">-->
-        <?php
-        echo TFuncoes::AddCss(true);
-        ?>
+        <?php echo TFuncoes::AddCss(true); ?>
     </head>
 
     <body class="skin-blue">
@@ -27,24 +22,23 @@ include './TFuncao.php';
             <!--Menu lateral-->
             <?php echo TFuncoes::AddMenuLateral(true); ?>
 
-            <!--centro-->
-            <div class="content-wrapper" style="min-height: 717px;">
-                <section class="content">
+            <div class="content-wrapper" style="min-height: 717px;"><!--INICIO DO CENTRO-->
 
-                    <!--Meio a ser mudado--> 
-                    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                        <h1 class="h2">Locação</h1>
-                        <div class="btn-toolbar mb-2 mb-md-0">
-                            <div class="btn-group mr-2">
-                                <a href="pages/cadastrar_locacao.php" class="btn btn-sm btn-outline-secondary">Cadastrar</a>
-                                <a class="btn btn-sm btn-outline-secondary">Deletar</a>
-                                <a class="btn btn-sm btn-outline-secondary">Editar</a>
+                    <!--**********************************************************-->
+
+                    <div class="container-fluid">
+
+                        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                            <h1 class="h2">Locações</h1>
+                            <div class="btn-toolbar mb-2 mb-md-0">
+                                <div class="btn-group mr-2">	
+                                    <a href="cadastrar_locacao.php" class="btn btn-success" data-toggle="modal" data-target="#modalCadastro">
+                                    <i class="fas fa-pen"></i>
+                                    Nova Locação</a>
+                                </div>
                             </div>
-                            <button class="btn btn-sm btn-outline-secondary dropdown-toggle">
-                                <span data-feather="calendar"></span>
-                                Hoje
-                            </button>
                         </div>
+
                     </div>
 
                     <div class="table-responsive">
@@ -174,14 +168,96 @@ include './TFuncao.php';
                             </tbody>
                         </table>
                     </div>
-                    <!--Final do centro aonde irá ocorrer Edição-->
-                </section>
-            </div>
-            <!--finalização do Centro--> 
 
-            <!--painel mudar de cor-->
-            <?php echo TFuncoes::AddPainelCor(); ?>
+                    <!-- INICIO DOS MODALS-->
+                    <div class="modal" id="modalCadastro"> <!--MODAL PARA CADASTRO-->
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+
+                                <div class="modal-header">
+                                    <h4 class="modal-title">Cadastrar Locação</h4>
+                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                </div>
+
+                                <div class="modal-body">
+                                    
+                                    <div class="col-6">
+                                        <form>
+                                            <div class="form-group">
+                                                <label for="numeroCliente">Número do Cliente</label>
+                                                <input type="text" class="form-control" id="numeroCliente">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="placaCarro">Placa do Carro</label>
+                                                <input type="text" class="form-control" id="placaCarro">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="dataLocacao">Data de Locação</label>
+                                                <input type="date" class="form-control" id="dataLocacao" value="" >
+                                            </div>
+                                        </form>
+                                    </div>
+
+                                </div>
+
+                                <div class="modal-footer">
+                                    <button type="submit" class="btn btn-success">Cadastrar</button>
+                                    <button type="button" class="btn btn-danger" data-dismiss="modal">Fechar</button>
+                                </div>
+
+                            </div>
+                        </div>
+				    </div>
+
+                    <div class="modal" id="modalEditar"> <!--MODAL PARA EDIÇÃO-->
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+
+                                <div class="modal-header">
+                                    <h4 class="modal-title">Editar Locação</h4>
+                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                </div>
+
+                                <div class="modal-body">
+                                    
+                                    <div class="col-6">
+                                        <form>
+                                            <div class="form-group">
+                                                <label for="numeroCliente">Número do Cliente</label>
+                                                <input type="text" class="form-control" id="numeroCliente">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="placaCarro">Placa do Carro</label>
+                                                <input type="text" class="form-control" id="placaCarro">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="dataLocacao">Data de Locação</label>
+                                                <input type="date" class="form-control" id="dataLocacao" value="" >
+                                            </div>
+                                        </form>
+                                    </div>
+
+                                </div>
+
+                                <div class="modal-footer">
+                                    <button type="submit" class="btn btn-success">Salvar</button>
+                                    <button type="button" class="btn btn-danger" data-dismiss="modal">Fechar</button>
+                                </div>
+                                
+                            </div>
+                        </div>
+				    </div>
+                    <!--FIM DOS MODALS-->
+
+                    <!--**********************************************************-->
+
+            </div><!--FIM DO CENTRO--> 
+
+            <?php echo TFuncoes::AddPainelCor(); ?> <!--PAINEL MUDAR DE COR-->
+
         </div>
-        <?php echo TFuncoes::AddJs(true) ?>
+
+        <?php echo TFuncoes::AddJs(true) ?> <!--INCLUIR JS-->
+
     </body>
 </html>
