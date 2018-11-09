e5e64ec4ac8ba0069e6f1d8e32d047e8c0fece4e
João Vitor Morosini <jvm-nogueira@hotmail.com>
2018-11-09 20:45:13 -0300

    Correção de layout e adição de botões

--- mobile/app/src/main/java/unigran/br/locvec/LocacaoManutencao.java
+++ mobile/app/src/main/java/unigran/br/locvec/LocacaoManutencao.java
@@ -1,7 +1,9 @@
 package unigran.br.locvec;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import locvec.unigran.br.locvec.R;
 
@@ -11,5 +13,21 @@ public class LocacaoManutencao extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_locacao_manutencao);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setTitle("Cadastro de Locação");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                startActivity(new Intent(this, ListaLocacao.class));
+                finishAffinity();
+                break;
+            default:break;
+        }
+        return true;
     }
 }
