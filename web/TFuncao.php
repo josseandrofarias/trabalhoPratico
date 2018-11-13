c2a7b49728716ce90351ef0901d5033f1030dc86
JosseandroFarias <farias.josseandro@gmail.com>
2018-11-13 14:11:39 -0400

    Atualização de caminhos de js (Jquery)

--- web/TFuncao.php
+++ web/TFuncao.php
@@ -110,13 +110,13 @@ abstract class TFuncoes {
                 <script src="./dados/js/bootbox.min.js" type="text/javascript"></script>
                 <!-- Icons -->
                 <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
-                <script>
+                <script>    
                     feather.replace()
                 </script>
                 <!-- Graphs -->
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js"></script>';
         } else {
-            return '<script src="./dados/js/jquery-3.3.1.min.js" type="text/javascript"></script>
+            return '<script src="../dados/js/jquery-3.3.1.min.js" type="text/javascript"></script>
                 <script src="../dados/js/bootstrap.min.js" type="text/javascript"></script>
                 <script src="../dados/js/adminlte.min.js" type="text/javascript"></script>
                 <script src="../dados/js/demo.js" type="text/javascript"></script>
