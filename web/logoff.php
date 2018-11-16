1b625e769abcdf4ebfde68ab26657d5e624188ad
JosseandroFarias <farias.josseandro@gmail.com>
2018-11-16 09:22:04 -0400

    Correação de caminho de redirecionamento de logoff

--- web/logoff.php
+++ web/logoff.php
@@ -2,6 +2,6 @@
 // Inicia sessões, para assim poder destruí-las 
 session_start();
 if(session_destroy()) {
-    header("Location: login.php");
+    header("Location: " . "http://" . "$_SERVER[HTTP_HOST]" . "/trabalhoPratico/web/login.php");
 }
 ?>
