a86d3ac05b29af114e084a307924778d20e51d7d
Paulo Wosniak <paulohwff@gmail.com>
2018-11-23 16:07:10 -0200

    Sistema de gerar relatorios implementado

--- mobile/app/src/main/java/unigran/br/locvec/Relatorios.java
+++ mobile/app/src/main/java/unigran/br/locvec/Relatorios.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -16,17 +17,24 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Toast;
 import unigran.br.locvec.DAO.Banco;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 import locvec.unigran.br.locvec.R;
+import unigran.br.locvec.DAO.DaoVeiculo;
 import unigran.br.locvec.Entidades.EFuncionario;
+import unigran.br.locvec.Entidades.ELocacao;
+import unigran.br.locvec.Entidades.EVeiculo;
+//import unigran.br.locvec.Entidades.ECliente;
 import unigran.br.locvec.Utilitarios.MáscaraCampoData;
 
 
@@ -34,10 +42,11 @@ public class Relatorios extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     Spinner tipoRelatorio;
+    Button btGerar;
     EditText dataInicial, dataFinal;
     static boolean active = false;
     Banco bd;
-    private ListView lista;
+    private ListView listRelatorio;
     private SQLiteDatabase conexao;
 
     @Override
@@ -46,44 +55,52 @@ public class Relatorios extends AppCompatActivity
         setContentView(R.layout.activity_relatorios);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-//        ArrayAdapter<EFuncionario> arrayAdapter = new ArrayAdapter<EFuncionario>(this, android.R.layout.simple_list_item_1, listar());
-//        lista.setAdapter(arrayAdapter);
-
-
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
         conexaoDB();
-//        listar();
-        lista = findViewById(R.id.listRelatorio);
-        // TIPOS DE RELATORIO
 
+        // Metodos para escolher o tipo do relatorio
         tipoRelatorio = (Spinner) findViewById(R.id.tipoRelatorio);
-
         ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.tiposRelatorio, android.R.layout.simple_spinner_item);
         tipoRelatorio.setAdapter(adapter);
 
-
-
-        // FIM TIPOS DE RELATORIO
-
-        // MASCARA PARA DATA INICIAL E FINAL
+        // Metodos para aplicar mascaras aos campos de data
         dataInicial = (EditText) findViewById(R.id.edDataInicial);
         new MáscaraCampoData(dataInicial);
-
         dataFinal = (EditText) findViewById(R.id.edDataFinal);
         new MáscaraCampoData(dataFinal);
 
+        btGerar = (Button) findViewById(R.id.btGerar);
+        listRelatorio = (ListView) findViewById(R.id.listRelatorio);
 
 
     }
+    // CONECTANDO NO BANCO
+    private void conexaoDB(){
+        try {
+            bd = new Banco(this);
+            Toast.makeText(this, "Conexão Ok", Toast.LENGTH_SHORT).show();
+        }catch (SQLException ex) {
+            AlertDialog.Builder msg = new AlertDialog.Builder(this);
+            msg.setTitle("Erro");
+            msg.setMessage("Erro de Conexao");
+            msg.setNeutralButton("Ok", null);
+            msg.show();
+        }
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+
+    }
 
     @Override
     public void onBackPressed() {
@@ -128,9 +145,9 @@ public class Relatorios extends AppCompatActivity
     public void onStop() {
         super.onStop();
         active = false;
-        finish();
+        finishAffinity();
     }
-
+    // MENU LATERAL
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
@@ -164,61 +181,139 @@ public class Relatorios extends AppCompatActivity
         return true;
     }
 
-    private void conexaoDB(){
-        try {
-            bd = new Banco(this);
-            Toast.makeText(this, "Conexão Ok", Toast.LENGTH_SHORT).show();
-        }catch (SQLException ex) {
-            AlertDialog.Builder msg = new AlertDialog.Builder(this);
-            msg.setTitle("Erro");
-            msg.setMessage("Erro de Conexao");
-            msg.setNeutralButton("Ok", null);
-            msg.show();
-        }
-    }
 
-//    private List listar(){
-//        conexao = bd.getWritableDatabase();
-//        List funcionario = new LinkedList();
-//        Cursor res = conexao.rawQuery("SELECT * FROM funcionario", null);
-//        if(res.getCount() > 0){
-//            res.moveToFirst();
-//            do {
-//                EFuncionario efunc = new EFuncionario();
-//                efunc.setvNome(res.getString(res.getColumnIndexOrThrow("nome")));
-//                //efunc.setvFlagDesativado(res.getWantsAllOnMoveCalls(res.getColumnIndexOrThrow("desativado")));
-//                funcionario.add(efunc);
-//            } while (res.moveToNext());
-//        }
-//        return funcionario;
-//    }
-
+    // AO CLICAR NO BOTÃO SERÁ VERIFICADO O TIPO DE RELATORIO SELECIONADO
     public void clickBtnGerar(View view) {
 
         switch ((String) tipoRelatorio.getSelectedItem()){
 
             case "Carros":
 
+                ArrayAdapter<EVeiculo> arrayAdapter = new ArrayAdapter<EVeiculo>(this, android.R.layout.simple_list_item_1, listaRelatorio("carro"));
+                listRelatorio.setAdapter(arrayAdapter);
+
                 Toast.makeText(this, ""+tipoRelatorio.getSelectedItem()+" "+dataInicial.getText()+" "+dataFinal.getText(), Toast.LENGTH_SHORT).show();
                 break;
             case "Locações":
+                ArrayAdapter<ELocacao> eLocacaoArrayAdapter = new ArrayAdapter<ELocacao>(this, android.R.layout.simple_list_item_1, listaRelatorio("locacao"));
+                listRelatorio.setAdapter(eLocacaoArrayAdapter);
+
                 Toast.makeText(this, "Locações", Toast.LENGTH_SHORT).show();
                 break;
             case "Clientes":
+//                ArrayAdapter<ECliente> eClienteArrayAdapter = new ArrayAdapter<ECliente>(this, android.R.layout.simple_list_item_1, listaRelatorio("cliente"));
+//                listRelatorio.setAdapter(eClienteArrayAdapter);
                 Toast.makeText(this, "Clientes", Toast.LENGTH_SHORT).show();
                 break;
             case "Funcionários":
+                ArrayAdapter<EFuncionario> eFuncionarioArrayAdapter = new ArrayAdapter<EFuncionario>(this, android.R.layout.simple_list_item_1, listaRelatorio("funcionario"));
+                listRelatorio.setAdapter(eFuncionarioArrayAdapter);
                 Toast.makeText(this, "Funcionario", Toast.LENGTH_SHORT).show();
                 break;
 
         }
 
     }
-//
-//    @Override
-//    protected void onResume() {
-//        super.onResume();
-//        ArrayAdapter<Relatorios> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listar());
-//        lista.setAdapter(arrayAdapter);
-//    }
+
+    //APÓS VERIFICAR O TIPO SELECIONADO É FEITA A CONSULTA NO BANCO E LOGO EM SEGUIDA A LISTAGEM
+    public List listaRelatorio(String tipo){
+        conexao = bd.getReadableDatabase();
+        List dados = new LinkedList();
+        Cursor res;
+        switch (tipo){
+            case "carro":
+
+                 res = conexao.rawQuery("SELECT * FROM carro WHERE dataCad >= "+dataInicial+" && dataCad <= "+ dataFinal,null);
+                if(res.getCount()>0){
+                    res.moveToFirst();
+                    do{
+                        EVeiculo eveic = new EVeiculo();
+                        eveic.setId(res.getInt(res.getColumnIndexOrThrow("ID")));
+                        eveic.setNome(res.getString(res.getColumnIndexOrThrow("NOME")));
+                        eveic.setModelo(res.getString(res.getColumnIndexOrThrow("MODELO")));
+                        eveic.setPlaca(res.getString(res.getColumnIndexOrThrow("PLACA")));
+                        eveic.setValorSeguro(res.getFloat(res.getColumnIndexOrThrow("SEGURO")));
+                        eveic.setValorLocacao(res.getFloat(res.getColumnIndexOrThrow("LOCAÇÃO")));
+                        eveic.setCor(res.getString(res.getColumnIndexOrThrow("COR")));
+                        eveic.setMarca(res.getString(res.getColumnIndexOrThrow("MARCA")));
+                        dados.add(eveic);
+                    }while (res.moveToNext());
+                }else
+                    msgErroGerar();
+                break;
+
+
+            case "cliente":
+                res = conexao.rawQuery("SELECT * FROM cliente WHERE dataCad >= "+dataInicial+" && dataCad <= "+ dataFinal,null);
+                if(res.getCount()>0){
+                    res.moveToFirst();
+                    do{
+//                        ECliente eCliente = new ECliente();
+//                        eCliente.setId(res.getInt(res.getColumnIndexOrThrow("ID")));
+//                        eCliente.setNome(res.getString(res.getColumnIndexOrThrow("NOME")));
+//                        eCliente.setCpf(res.getString(res.getColumnIndexOrThrow("CPF")));
+//                        eCliente.setRg(res.getString(res.getColumnIndexOrThrow("RG")));
+//                        eCliente.setCnh(res.getString(res.getColumnIndexOrThrow("CNH")));
+//                        eCliente.setEndereco(res.getString(res.getColumnIndexOrThrow("ENDEREÇO")));
+//                        eCliente.setNumeroDependentes(res.getInt(res.getColumnIndexOrThrow("DEPENDENTES")));
+//                        dados.add(eCliente);
+                    }while (res.moveToNext());
+                }else
+                    msgErroGerar();
+                break;
+
+            case "funcionario":
+                res = conexao.rawQuery("SELECT * FROM funcionario WHERE dataAdmissao >= "+dataInicial+" && dataAdmissao <= "+ dataFinal,null);
+                if(res.getCount()>0){
+                    res.moveToFirst();
+                    do{
+                        EFuncionario eFuncionario = new EFuncionario();
+                        eFuncionario.setId(res.getInt(res.getColumnIndexOrThrow("ID")));
+                        eFuncionario.setvNome(res.getString(res.getColumnIndexOrThrow("NOME")));
+                        eFuncionario.setvCPF(res.getString(res.getColumnIndexOrThrow("CPF")));
+                        eFuncionario.setvRG(res.getString(res.getColumnIndexOrThrow("RG")));
+                        eFuncionario.setvEndereco(res.getString(res.getColumnIndexOrThrow("ENDERECO")));
+//                        eFuncionario.setvAdmissao(res.getString(res.getColumnIndexOrThrow("DATA ADMISSAO")));
+//                        eFuncionario.setvDemissao(res.getString(res.getColumnIndexOrThrow("DATA DEMISSAO")));
+                        dados.add(eFuncionario);
+                    }while (res.moveToNext());
+                }else
+                    msgErroGerar();
+                break;
+
+            case "locacao":
+                res = conexao.rawQuery("SELECT a.id, a.dataLocacao, a.dataDevolucao, a.quilometragem, b.nome, b.cnh, c.nome, c.placa FROM locacao AS a INNER JOIN cliente b ON a.idCliente = b.id INNER JOIN carro c ON c.id = a.idCarro",null);
+                if(res.getCount()>0){
+                    res.moveToFirst();
+                    do{
+                        ELocacao eLocacao = new ELocacao();
+                        eLocacao.setId(res.getInt(res.getColumnIndexOrThrow("ID")));
+//                        eLocacao.setDataLocacao(res.getString(res.getColumnIndexOrThrow("DATA LOCAÇÃO")));
+//                        eLocacao.setDataDevolucao(res.getString(res.getColumnIndexOrThrow("DATA DEVOLUÇÃO")));
+                        eLocacao.setQuilometragem(res.getFloat(res.getColumnIndexOrThrow("QUILOMETRAGEM")));
+//                        eLocacao.setNome(res.getString(res.getColumnIndexOrThrow("NOME")));
+//                        eLocacao.setCnh(res.getString(res.getColumnIndexOrThrow("CNH")));
+//                        eLocacao.setNome(res.getString(res.getColumnIndexOrThrow("CARRO")));
+//                        eLocacao.setPlaca(res.getString(res.getColumnIndexOrThrow("PLACA")));
+                        dados.add(eLocacao);
+                    }while (res.moveToNext());
+                }else
+                    msgErroGerar();
+                break;
+
+
+        }
+                conexao.close();
+                return dados;
+
+
+    }
+
+    private void msgErroGerar(){
+        AlertDialog.Builder msg = new AlertDialog.Builder(this);
+        msg.setTitle("Erro");
+        msg.setMessage("Não possui dados Cadastrados!");
+        msg.setNeutralButton("Ok" ,null);
+        msg.show();
+    }
 }
