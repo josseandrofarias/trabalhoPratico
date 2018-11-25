0227eac7d0912953b101a96e49f2459a857ff11b
Alex <802.099@alunos.unigran.br>
2018-11-25 15:26:27 -0300

    ajuste de layout, edição e inserção de funcionarios

--- web/pages/del_funcionario.php
+++ web/pages/del_funcionario.php
@@ -4,22 +4,19 @@
 
     $con = TFuncoes::AddConexao();
 
-   $sql = "DELETE FROM funcionario WHERE ID=".$id.";";
+    $sql = "DELETE FROM funcionario WHERE ID=".$id.";";
 
     echo '<br>';
     
     if(mysqli_query($con, $sql)){
         $msg = "Gravado com sucesso!";
-        header('Location: funcionario.php');
+        header('Location: cad_funcionario.php');
         mysqli_close($con);  
     }else{
         $msg = "Erro ao gravar!";
         echo "
-        <script>alert('O veículo possui uma locação ativa e não pode ser excluído.');
-        window.location.href = 'veiculo.php'  //redirecionar para a pagina de veiculo
-        
+        window.location.href = 'cad_funcionario.php'  //redirecionar para a pagina de veiculo
         </script>";
-       
     }
     mysqli_close($con);  
     
